2c175cd16ec7b9c2284564019d995ee95ab76147
KevinCTofel <ktofel3811@students.mc3.edu>
2020-07-23 13:51:10 -0400

    another small fix to next page

--- src/templates/post-list.js
+++ src/templates/post-list.js
@@ -15,7 +15,7 @@ const PostList = (props) => {
     const isFirst = currentPage === 1
     const isLast = currentPage === numPages
     const prevPage = currentPage - 1 === 1 ? "/" : (currentPage - 1).toString()
-    const nextPage = "/$(currentPage + 1).toString()"
+    const nextPage = "/" + (currentPage + 1).toString()
 
     const getTechTags = (tags) => {
         const techTags = []
