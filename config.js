80b12cdfcf4694a92c00ff6851ac061841d56fa2
Kevin C. Tofel <kctofel@gmail.com>
2020-08-08 09:52:24 -0400

    New post

--- config.js
+++ config.js
@@ -128,5 +128,12 @@ module.exports = {
       size: 20,
       color: "crimson",
     },
+    {
+      tag: "windows",
+      tech: "Windows",
+        name: "M0 3.449L9.75 2.1v9.451H0m10.949-9.602L24 0v11.4H10.949M0 12.6h9.75v9.451L0 20.699M10.949 12.6H24V24l-12.9-1.801", 
+      size: 20,
+      color: "deepskyblue",
+    },
   ],
 }
