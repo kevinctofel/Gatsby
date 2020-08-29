e0728f4edf1ad4213f76eddc1f4792641974e2cc
KevinCTofel <ktofel3811@students.mc3.edu>
2020-08-29 10:45:03 -0400

    Adding RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    'gatsby-plugin-feed',
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
