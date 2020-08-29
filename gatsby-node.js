888f25bb8599a5cdb56654708598497731397bdd
KevinCTofel <ktofel3811@students.mc3.edu>
2020-08-29 10:49:55 -0400

    Add module for RSS support

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,6 +2,18 @@ const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const _ = require("lodash")
 
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
